8b194ac3feac117d7d1b6453c4145ce6f29cbc63
sujin16 <tnwls9712@ajou.ac.kr>
2019-07-10 19:27:49 +0900

    Update UpperCaseCounterTest.java

--- src/test/java/UpperCaseCounterTest.java
+++ src/test/java/UpperCaseCounterTest.java
@@ -2,7 +2,10 @@ import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+import static org.hamcrest.Matchers.*;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
@@ -10,20 +13,34 @@ public class UpperCaseCounterTest {
 
     private UpperCaseCounter upperCaseCounter = new UpperCaseCounter();
 
-    //null을 전달했을 때 0을 리턴하는지에 대한 테스트 작성
     @Test
     public void getNumberOfUpperCaseCharactersInString_return_0_for_null_input(){
         String str = null;
-
-//        int numberOfUpperCaseCharactersInString = upperCaseCounter.getNumberOfUpperCaseCharactersInString(str);
+        int numberOfUpperCaseCharactersInString = upperCaseCounter.getNumberOfUpperCaseCharactersInString(str);
+        assertTrue(numberOfUpperCaseCharactersInString ==0);
     }
 
-    //반겂울 전달했을 때 0을 리턴하는지에 대한 테스트 작성
+    @Test //new Method (1)
+    public void given2Strings_whenEqual_thenCorrect() {
+        String a = "foo";
+        String b = "FOO";
+        assertThat(a, equalToIgnoringCase(b));
+    }
+
+    @Test //new Method (2)
+    public void given2Strings() {
+        String a = "foo";
+        String b = "FOO";
+        assertThat(a,is(b));
+    }
+
+    //반값울 전달했을 때 0을 리턴하는지에 대한 테스트 작성
     @Test
     public void getNumberOfUpperCaseCharactersInString_return_0_for_empty_input() {
         String str = "";
 
-//        int numberOfUpperCaseCharactersInString = upperCaseCounter.getNumberOfUpperCaseCharactersInString(str);
+        int numberOfUpperCaseCharactersInString = upperCaseCounter.getNumberOfUpperCaseCharactersInString(str);
+        //assertTrue(numberOfUpperCaseCharactersInString ==0);
     }
 
     //대문자들이 포함된 문자열을 전달했을 때 카운팅된 숫자와 맞는지 검증하는 테스트 작성
@@ -34,8 +51,11 @@ public class UpperCaseCounterTest {
         int numberOfUpperCaseCharactersInString = upperCaseCounter.getNumberOfUpperCaseCharactersInString(str);
 
         //assertTrue로 맞는 테스트 코드 작성
+        //assertTrue(numberOfUpperCaseCharactersInString ==10);
         //assertFalse로 틀리는 값을 넣어 테스트 작성
+        // assertFalse(numberOfUpperCaseCharactersInString ==9);
         //assertThat 단정문을 사용해서 True인 테스트 코드 작성
+        //assertThat(numberOfUpperCaseCharactersInString,is(10));
     }
 
     //대소문자가 섞여 있을 때 정확히 카운팅 되는 지에 대한 테스트 코드 작성
@@ -44,20 +64,59 @@ public class UpperCaseCounterTest {
         String str = "ABCdefGHI";
         int result = upperCaseCounter.getNumberOfUpperCaseCharactersInString(str);
         //assertTrue로 맞는 테스트 코드 작성
+        //assertTrue(result ==6);
         //assertFalse로 틀리는 값을 넣어 테스트 작성
+        //assertFalse(result==4);
         //assertThat 단정문을 사용해서 True인 테스트 코드 작성
+        //assertThat(result,is(6));
+        System.out.println("result :: " + result);
+    }
+
+    @Test //new Method(3)
+    public void getNumberOfLowerCaseCharacterInString_return_4(){
+        String str1 = "ABCdefGHI";
+        String str2 = "ABCDEFGHIJ";
+        String str3 = "abcdefghij";
+
+        int result = upperCaseCounter.getNumberOfUpperCaseCharactersInString(str2)-
+                upperCaseCounter.getNumberOfUpperCaseCharactersInString(str1);
+        int test_result =upperCaseCounter.getNumberOfUpperCaseCharactersInString(str3);
+        assertTrue(test_result == 0);
+        System.out.println("test_result :: " + test_result);
+        //System.out.println("result :: " + result);
+
+    }
+
+    @Test//new Method(4)
+    public void givenArray_allLowerCase_result_0() {
+        int result=0;
+        String[] array = {"One", "Two","THREE"};
+        for(int i=0;i<array.length;i++){
+            array[i] =  array[i].toLowerCase();
+            result =+ upperCaseCounter.getNumberOfUpperCaseCharactersInString(array[i]);
+        }
+
+        assertThat(result, is(0));
         System.out.println("result :: " + result);
     }
 
     //잘못된 값을 참조했을 때 IndexOutOfBoundsException Exception이 발생하는지 테스트 코드 작성
-    @Test
+    @Test(expected = IndexOutOfBoundsException.class)
     public void shouldThrowExceptionWhenGetZeroIndex() {
         new ArrayList<Object>().get(0);
     }
 
+
+    @Test(expected = ArithmeticException.class) //new Method(5)
+    public void shouldThrowExceptionWhenGetZero() {
+        new Integer(4/0);
+    }
+
+
+
     //해당 메소드가 제한된 시간내에 실행되는지에 대한 테스트 코드 작성 : timeout 사용
     //두번째로 해당 메소드는 테스트 하지 않도록 어노테이션 추가 적용 해봅니다. Ignore
-    @Test
+    @Test(timeout = 5000)
     public void testShouldRunInLimitedTime() throws InterruptedException {
         Thread.sleep(4000);
         System.out.println("제한된 시간 내에 수행되면 테스트 Passed!");
