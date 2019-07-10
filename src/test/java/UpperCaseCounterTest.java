c4c63c9eacab8f3dc3acf41254385e3a6a60c593
sujin16 <tnwls9712@ajou.ac.kr>
2019-07-10 19:45:11 +0900

    Delete Comments UpperCaseCounterTest

--- src/test/java/UpperCaseCounterTest.java
+++ src/test/java/UpperCaseCounterTest.java
@@ -60,7 +60,7 @@ public class UpperCaseCounterTest {
 
 
 
-    //null을 전달했을 때 0을 리턴하는지에 대한 테스트 작성
+
     @Test
     public void getNumberOfUpperCaseCharactersInString_return_0_for_null_input(){
         String str = null;
@@ -68,7 +68,7 @@ public class UpperCaseCounterTest {
 //        int numberOfUpperCaseCharactersInString = upperCaseCounter.getNumberOfUpperCaseCharactersInString(str);
     }
 
-    //반겂울 전달했을 때 0을 리턴하는지에 대한 테스트 작성
+
     @Test
     public void getNumberOfUpperCaseCharactersInString_return_0_for_empty_input() {
         String str = "";
@@ -76,7 +76,7 @@ public class UpperCaseCounterTest {
 //        int numberOfUpperCaseCharactersInString = upperCaseCounter.getNumberOfUpperCaseCharactersInString(str);
     }
 
-    //대문자들이 포함된 문자열을 전달했을 때 카운팅된 숫자와 맞는지 검증하는 테스트 작성
+
     @Test
     public void getNumberOfUpperCaseCharactersInString_return_10_for_ABCDEFGHIJ() {
         String str = "ABCDEFGHIJ";
@@ -88,7 +88,7 @@ public class UpperCaseCounterTest {
         //assertThat 단정문을 사용해서 True인 테스트 코드 작성
     }
 
-    //대소문자가 섞여 있을 때 정확히 카운팅 되는 지에 대한 테스트 코드 작성
+
     @Test
     public void getNumberOfUpperCaseCharacterInString_return_6_for_ABCdefGHI(){
         String str = "ABCdefGHI";
@@ -99,14 +99,13 @@ public class UpperCaseCounterTest {
         System.out.println("result :: " + result);
     }
 
-    //잘못된 값을 참조했을 때 IndexOutOfBoundsException Exception이 발생하는지 테스트 코드 작성
+
     @Test
     public void shouldThrowExceptionWhenGetZeroIndex() {
         new ArrayList<Object>().get(0);
     }
 
-    //해당 메소드가 제한된 시간내에 실행되는지에 대한 테스트 코드 작성 : timeout 사용
-    //두번째로 해당 메소드는 테스트 하지 않도록 어노테이션 추가 적용 해봅니다. Ignore
+
     @Test
     public void testShouldRunInLimitedTime() throws InterruptedException {
         Thread.sleep(4000);
