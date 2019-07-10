62e3ebb59c270c89c6f0295b6c44dd444e6d7a2d
sujin16 <tnwls9712@ajou.ac.kr>
2019-07-10 19:43:25 +0900

    Update UpperCaseCounterTest.java

--- src/test/java/UpperCaseCounterTest.java
+++ src/test/java/UpperCaseCounterTest.java
@@ -3,12 +3,62 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 
-import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
 
 public class UpperCaseCounterTest {
 
     private UpperCaseCounter upperCaseCounter = new UpperCaseCounter();
+
+    @Test //new Method (1)
+    public void given2Strings_whenEqual_thenCorrect() {
+        String a = "foo";
+        String b = "FOO";
+        assertThat(a, equalToIgnoringCase(b));
+    }
+
+    @Test //new Method (2)
+    public void given2Strings() {
+        String a = "foo";
+        String b = "FOO";
+        assertThat(a,is(b));
+    }
+
+    @Test //new Method(3)
+    public void getNumberOfLowerCaseCharacterInString_return_4(){
+        String str1 = "ABCdefGHI";
+        String str2 = "ABCDEFGHIJ";
+        String str3 = "abcdefghij";
+
+        int result = upperCaseCounter.getNumberOfUpperCaseCharactersInString(str2)-
+                upperCaseCounter.getNumberOfUpperCaseCharactersInString(str1);
+        int test_result =upperCaseCounter.getNumberOfUpperCaseCharactersInString(str3);
+        assertTrue(test_result == 0);
+        System.out.println("test_result :: " + test_result);
+        //System.out.println("result :: " + result);
+
+    }
+
+    @Test//new Method(4)
+    public void givenArray_allLowerCase_result_0() {
+        int result=0;
+        String[] array = {"One", "Two","THREE"};
+        for(int i=0;i<array.length;i++){
+            array[i] =  array[i].toLowerCase();
+            result =+ upperCaseCounter.getNumberOfUpperCaseCharactersInString(array[i]);
+        }
+
+        assertThat(result, is(0));
+        System.out.println("result :: " + result);
+    }
+
+    @Test(expected = ArithmeticException.class) //new Method(5)
+    public void shouldThrowExceptionWhenGetZero() {
+        new Integer(4/0);
+    }
+
+
+
 
     //null을 전달했을 때 0을 리턴하는지에 대한 테스트 작성
     @Test
